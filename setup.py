783e350058c5d3d61764c743b8558f6508901960
Ian Neidel <68819605+ianneidel@users.noreply.github.com>
2023-03-06 22:28:30 -0500

    make runSW available from package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='smithwaterman',
-    version='1.1.4',
+    version='1.1.5',
     author='Ian Neidel',
     author_email='ian.neidel@yale.edu',
     description='An implementation of the Smith-Waterman algorithm, supporting an affine gap penalty',
--- smithwaterman/__init__.py
+++ smithwaterman/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['runSW']
+from .hw1 import runSW
