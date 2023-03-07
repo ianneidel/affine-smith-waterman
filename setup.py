cd6fa9ff88d858fb08e08626ea33fc0b1e5d3229
Ian Neidel <68819605+ianneidel@users.noreply.github.com>
2023-03-06 22:11:35 -0500

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='smithwaterman',
-    version='1.0',
+    version='1.1',
     author='Ian Neidel',
     author_email='ian.neidel@yale.edu',
     description='An implementation of the Smith-Waterman algorithm, supporting an affine gap penalty',
